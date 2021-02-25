c03c3f8758ae963dfe9cab6cac13ce1bdc4ba3d2
sandraelsa16 <73426909+sandraelsa16@users.noreply.github.com>
2021-02-25 12:28:35 +0530

    Add files via upload

---
+++ Division.java
@@ -0,0 +1,39 @@
+import java.util.*;
+
+public class Division
+{
+
+     
+public static void main(String []args){
+
+       float a,b;
+
+       float c;
+
+       Scanner k=new Scanner(System.in);
+
+       System.out.println("Enter two numbers");
+
+       a=k.nextFloat();
+
+       b=k.nextFloat();
+
+       try{
+
+           c=a/b;
+
+           System.out.println("Ans:"+c);
+
+       }
+
+       catch(ArithmeticException e)
+
+       {
+
+           System.out.println("Wrong input"+e);
+
+       }
+
+     }
+   
+}
